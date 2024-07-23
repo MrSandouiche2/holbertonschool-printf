0cafd0383d7ad5faf616543d118481a08ca6a726
MrSandouiche2 <zemihimehdi1@gmail.com>
2024-07-23 16:47:35 +0200

    implemented basic _printf function to handle %c %s and %% specifiers

--- printf.c
+++ printf.c
@@ -4,6 +4,50 @@
 
 int _printf(const char *format, ...)
 {
+	va_list args;
+	va_start(args, format);
+	int counter = 0, i = 0;
 
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		if (format[i] == '%')
+		{
+			i++;
 
+			if (format[i] == 'c')
+			{
+				char c = va_arg(args, int);
+				putchar(c);
+				counter++;
+			}
+			else if (format[i] == 's')
+			{
+				char *s = va_arg(args, char *);
+				while (*s)
+				{
+					putchar(*s++);
+					counter++;
+				}
+			}
+			else if (format[i] == '%')
+			{
+				putchar('%');
+				counter++;
+			}
+			else
+			{
+				putchar('%');
+				putchar(format[i]);
+				counter += 2;
+			}
+		}
+		else
+		{
+			putchar(format[i]);
+			counter++;
+		}
+	}
+
+	va_end(args);
+	return (counter);
 }
