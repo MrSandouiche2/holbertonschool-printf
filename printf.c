1ea7a26555ed6ad503e1a63fcd7a74a3df79a463
MrSandouiche2 <zemihimehdi1@gmail.com>
2024-07-24 09:31:34 +0200

    Fixed compatibility program with C89

--- printf.c
+++ printf.c
@@ -3,14 +3,16 @@
 #include <stdarg.h>
 #include <unistd.h>
 
-
 int _printf(const char *format, ...)
 {
 	int chara_print = 0;
 	va_list args;
+
 	if (format == NULL)
 		return -1;
+
 	va_start(args, format);
+
 	while (*format)
 	{
 		if (*format != '%')
@@ -23,6 +25,7 @@ int _printf(const char *format, ...)
 			format++;
 			if (*format == '\0')
 				break;
+
 			if (*format == '%')
 			{
 				write(1, format, 1);
@@ -30,22 +33,36 @@ int _printf(const char *format, ...)
 			}
 			else if (*format == 'c')
 			{
-				char c = (char)va_arg(args, int);
+				char c;
+				c = (char)va_arg(args, int);
 				write(1, &c, 1);
 				chara_print++;
 			}
 			else if (*format == 's')
 			{
-				char *str = va_arg(args, char *);
-				int str_len = 0;
+				char *str;
+				int str_len;
+				str = va_arg(args, char *);
+				if (str == NULL)
+				{
+					str = "(null)";
+				}
+				str_len = 0;
 				while (str[str_len] != '\0')
 					str_len++;
 				write(1, str, str_len);
 				chara_print += str_len;
 			}
+			else
+			{
+				write(1, "%", 1);
+				write(1, format, 1);
+				chara_print += 2;
+			}
 		}
 		format++;
 	}
+
 	va_end(args);
 	return chara_print;
 }
